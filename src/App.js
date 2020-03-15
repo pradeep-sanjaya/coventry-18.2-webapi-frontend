40a94656bea106ce413b5677fca006e0b7290ea7
Pradeep <pradeeps@techprovint.com>
2020-03-15 13:30:15 +0530

    top navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import Cart from "./components/Cart.jsx";
+import ProductDetails from "./components/ProductDetails";
+import NewArrivals from "./components/NewArrivals";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="app">
-
+    <div className="site-wrap">
+      <BrowserRouter>
+        <React.Fragment>
+          <Navigation />
+          <Route exact path="/products" component={Products}></Route>
+          <Route exact path="/cart" component={Cart}></Route>
+          <Route exact path="/product/:id" component={ProductDetails}></Route>
+          <Route exact path="/new-arrivals" component={NewArrivals}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
+        </React.Fragment >
+      </BrowserRouter >
     </div>
   );
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Products extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Products</h1>
+            </div>
+        );
+    }
+}
+
+export default Products;
