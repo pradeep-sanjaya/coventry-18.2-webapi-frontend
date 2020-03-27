ea39c794bf0242c9cf1d7eb1b90a5e6c786ef5a5
Pradeep <sanjayangp@gmail.com>
2020-03-27 11:17:15 +0530

    update config

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,13 +1,3 @@
-let getConfig = () => {
-    if (process.env.NODE_ENV === 'development') {
-        return {
-            API_BASE_URL: "http://localhost:4000/api/v1"
-        }
-    } else {
-        return {
-            API_BASE_URL: "https://webapi-backend.herokuapp.com/api/v1"
-        }
-    }
-}
-
-export const config = getConfig();
+export const config = {
+    API_BASE_URL: "https://webapi-backend.herokuapp.com/api/v1"
+};
